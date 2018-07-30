6e72bfebba5f4315d6a0c462c0b3f7bd6e929360
Viana <brunovianasp@gmail.com>
2018-07-30 12:43:11 -0300

    Creating Activities

---
+++ kotlinapp/app/src/main/java/sample/app/LoadingActivity.kt
@@ -0,0 +1,20 @@
+package sample.app
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_loading.*
+import kotlin.app.R
+
+class LoadingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
